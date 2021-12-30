3e72b05413efc3a2d45cdaf9a8385cc8a6069394
Dovydas Stirpeika <Dovy91@gmail.com>
2021-12-30 18:07:16 -0500

    asset pacage.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-// dev-builder play (web || node)
-    // doing this should automatically using defaults start from playground web || node
-
-// TODO: "playground will create some starter files to get you started on the project"
 // TODO: "if you want to specify a different set of starter files you can point to a directory that will copied from"
 // TODO: way to configure to run browser and nodejs congruently
 // TODO: browser & nodejs debug in vscode
-// nodejs.dev // TODO: add comments on every opts arg
 
 export * from './browser.dev';
 export * from './browser.play';
